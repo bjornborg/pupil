2d034c67a6e0561f0e02445c112144603af372ac
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2022-12-12 13:50:56 +0100

    Added ui field for image size variable in shared memory
    Added healthy flag for runtime control

--- pupil_src/shared_modules/video_capture/shared_memory.py
+++ pupil_src/shared_modules/video_capture/shared_memory.py
@@ -119,34 +119,6 @@ class BGRFrame(Uint8BufferFrame):
 #             return self._gray
 
 
-# class GrayFrame(Uint8BufferFrame):
-#     @property
-#     def depth(self) -> int:
-#         return 1
-
-#     @property
-#     def bgr(self) -> npt.NDArray[np.uint8]:
-#         try:
-#             return self._bgr
-#         except AttributeError:
-#             self._bgr = np.ascontiguousarray(np.dstack([self._buffer] * 3))
-#             return self._bgr
-
-#     @property
-#     def gray(self):
-#         return self._buffer
-
-#     def interpret_buffer(
-#         self, buffer: bytes, width: int, height: int
-#     ) -> npt.NDArray[np.uint8]:
-#         array = super().interpret_buffer(buffer, width, height)
-#         # since this will be our gray buffer, we need to get rid of our third dimension
-#         array.shape = height, width
-#         return array
-
-
-# FRAME_CLASS_BY_FORMAT = {"rgb": RGBFrame, "bgr": BGRFrame, "gray": GrayFrame}
-
 class Shared_Memory(Base_Source):
     """
     Shared memory consumer for reading image data in /tmp
@@ -180,6 +152,8 @@ class Shared_Memory(Base_Source):
     ):
         super().__init__(g_pool, *args, **kwargs)
         self.fps = 0 
+        self.healthy = True
+        self.frame_size = (1280,720)
         self.startUnixTimestampNs = 0
         self.previousTimestampUnixNs = 0
         self.syncedPupilTime = self.g_pool.get_timestamp() #add this to normalised time
@@ -190,6 +164,7 @@ class Shared_Memory(Base_Source):
           self.previousTimestampUnixNs = self.shm.currentUnixTimestampNs
         else:
           self.shm = None
+          self.healthy = False
         
         self._intrinsics = Dummy_Camera(key, self.frame_size) # 2022-11-25 14:49:44 bb | We dont have support for this atm 
         # self.g_pool.capture = self
@@ -201,6 +176,9 @@ class Shared_Memory(Base_Source):
 
     # 2022-11-24 12:21:41 bb | This is main function that is called for every frame grab  
     def recent_events(self, events):
+        if not self.healthy:
+            return
+        
         frame = self.get_frame()
         if frame:
             events["frame"] = frame
@@ -212,42 +190,25 @@ class Shared_Memory(Base_Source):
           try:
             buf = self.shm.read(timeout=1) # timout in seconds
             # 2022-11-25 11:02:45 bb | Pupil labs (PyAv) does not support alpha channel
-            # width,height = self.frame_size()
-            # channels = 4
-            # imgBGR = np.frombuffer(buf, np.uint8).reshape(self.frame_size[1], self.frame_size[0], channels)[:,:,:3:-1]
-            # frame: SerializedFrame = None
-            # frame_class = FRAME_CLASS_BY_FORMAT["bgr"]
             # 2022-11-25 13:09:55 bb | remove the alpha channel
-            
-            self.fps = 10**9/(self.shm.currentUnixTimestampNs - self.previousTimestampUnixNs)
-            self.previousTimestampUnixNs = self.shm.currentUnixTimestampNs
-            puplTimestampSeconds = self.syncedPupilTime + (self.shm.currentUnixTimestampNs - self.startUnixTimestampNs)/10**9
-            return BGRFrame(buf,puplTimestampSeconds,self.shm.index(),self.frame_size[0], self.frame_size[1])
           except sysv_ipc.ExistentialError:
             # Error
             logger.error("sysv_ipc.ExistentialError")
-            self.shm.cleanup()
-            self.shm = None
+            self.healthy = False
             self._recent_frame = None
           except KeyError as err:
               logger.debug(f"Ill-formatted frame received. Missing key: {err}")
 
-        # if self.frame_sub.socket.poll(timeout=50):  # timeout in ms (50ms -> 20fps)
-        #     num_frames_dropped = -1
-        #     frame: SerializedFrame = None
-        #     while self.frame_sub.new_data:  # drop all but the newest frame
-        #         num_frames_dropped += 1
-        #         frame = self.frame_sub.recv()[1]
-        #     if num_frames_dropped and self.g_pool.process == "world":
-        #         logger.debug(f"Number of dropped frames: {num_frames_dropped}")
-
-        #     try:
-        #         frame_format = frame["format"]
-        #         if frame_format in FRAME_CLASS_BY_FORMAT:
-        #             frame_class = FRAME_CLASS_BY_FORMAT[frame_format]
-        #             return self._process_frame(frame_class, frame)
-        #     except KeyError as err:
-        #         logger.debug(f"Ill-formatted frame received. Missing key: {err}")
+          
+            
+          self.fps = 10**9/(self.shm.currentUnixTimestampNs - self.previousTimestampUnixNs)
+          self.previousTimestampUnixNs = self.shm.currentUnixTimestampNs
+          puplTimestampSeconds = self.syncedPupilTime + (self.shm.currentUnixTimestampNs - self.startUnixTimestampNs)/10**9
+          if(self.frame_size[0] * self.frame_size[1] * 4 != len(buf)):
+            logger.error("Incorrect image dimension:{},{}. Size of buf {}".format(self.frame_size[0],self.frame_size[1],len(buf)))
+            self.healthy = False
+          else:
+            return BGRFrame(buf,puplTimestampSeconds,self.shm.index(),self.frame_size[0], self.frame_size[1])
  
  
     @property
@@ -259,13 +220,14 @@ class Shared_Memory(Base_Source):
       
     @property
     def frame_size(self):
-      return (1280,720)
-        # return (
-        #     (self._recent_frame.width, self._recent_frame.height)
-        #     if self._recent_frame
-        #     # else (1440,1600) # 2022-11-24 11:08:23 bb | Htc vive pro, one eye
-        #     else (1280,720) # 2022-11-24 14:17:49 bb | to be removed
-        # )
+      return self._frame_size
+    
+    @frame_size.setter
+    def frame_size(self, size):
+      if(size[0]<1 or size[1]<1):
+        logger.error("Setting incorrect image size")
+      self.healthy = True
+      self._frame_size=size
 
     @property
     def frame_rate(self):
@@ -276,6 +238,14 @@ class Shared_Memory(Base_Source):
         return False
     
     
+    @property
+    def healthy(self):
+        return self._healthy
+      
+    @healthy.setter
+    def healthy(self, val):
+        self._healthy = val
+        
     @property
     def online(self):
         return self._recent_frame is not None
@@ -290,42 +260,16 @@ class Shared_Memory(Base_Source):
         self.shm.cleanup()
       self.shm = None
 
+    def ui_elements(self):
+        ui_elements = []
+        ui_elements.append(ui.Info_Text(f"Image size"))
+      
+        def set_frame_size(new_size):
+            self.frame_size = [int(ele) for ele in new_size.split(',')]
+                
+        ui_elements.append(ui.Text_Input("frame_size", self, label="(width,height)"))
 
-
-# class Base_Manager(Plugin):
-#     """Abstract base class for source managers.
-
-#     Managers are plugins that enumerate and load accessible sources from different
-#     backends, e.g. locally USB-connected cameras.
-
-#     Supported sources can be either single cameras or whole devices. Identification and
-#     activation of sources works via SourceInfo (see below).
-#     """
-
-#     # backend managers are always loaded and need to be loaded before the sources
-#     order = -1
-
-#     def __init__(self, g_pool):
-#         super().__init__(g_pool)
-
-#         # register all instances in g_pool.source_managers list
-#         if not hasattr(g_pool, "source_managers"):
-#             g_pool.source_managers = []
-
-#         if self not in g_pool.source_managers:
-#             g_pool.source_managers.append(self)
-
-#     def get_devices(self) -> T.Sequence["SourceInfo"]:
-#         """Return source infos for all devices that the backend supports."""
-#         return []
-
-#     def get_cameras(self) -> T.Sequence["SourceInfo"]:
-#         """Return source infos for all cameras that the backend supports."""
-#         return []
-
-#     def activate(self, key: T.Any) -> None:
-#         """Activate a source (device or camera) by key from source info."""
-#         pass
+        return ui_elements
 
 class Shared_Memory_Manager(Base_Manager):
     """Manages local shared memory sources."""
@@ -375,22 +319,4 @@ class Shared_Memory_Manager(Base_Manager):
         # self.devices.cleanup()
         self.shmList = None
 
-
-# class SourceInfo:
-#     """SourceInfo is a proxy for a source (camera or device) from a manager.
-
-#     Managers hand out source infos that can be activated from other places in the code.
-#     A manager needs to identify a source uniquely by a key.
-#     """
-
-#     def __init__(self, label: str, manager: Base_Manager, key: T.Any):
-#         self.label = label
-#         self.manager = manager
-#         self.key = key
-
-#     def activate(self) -> None:
-#         self.manager.activate(self.key)
-
-#     def __str__(self) -> str:
-#         return f"{self.label} - {self.manager.class_name}({self.key})"
 
